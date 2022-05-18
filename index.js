f84ce6a9a91224f8409c444d3961b7051923124e
roseokpe <roseokpe@gmail.com>
2022-05-18 16:19:11 +0100

    Added style and made correction in js file

--- index.js
+++ index.js
@@ -2,29 +2,30 @@ const hamburger = document.querySelector('.hamburger');
 // const mobileMenu = document.querySelector('');
 // const aboutContent = document.querySelector('');
 // const modal = document.querySelector('');
-hamburger.addEventListener('click', () =>{
-  hamburger.classList.toggle('active');
-  
+  hamburger.addEventListener('click', () =>{
+  //hamburger.classList.toggle('active');
+  document.querySelector('.mobile-menu').style.display = 'block';
 });
 
 function showMenu() {
   // document.querySelector('#menuList').style.display = 'inline-block';
   // document.querySelector('.mobile-sectionLinks').style.display = 'flex';
-  document.querySelector('.logo').style.display = 'none';
-  document.querySelector('.section-container').style.display = 'none';
-  document.querySelector('.work-container').style.display = 'none';
-  document.querySelector('.contact-container').style.display = 'none';
-  document.querySelector('.footer').style.display = 'none';
+  // document.querySelector ('.logo').style.display = 'none';
+  // document.querySelector('.section-container').style.display = 'none';
+  // document.querySelector('.work-container').style.display = 'none';
+  // document.querySelector('.contact-container').style.display = 'none';
+  // document.querySelector('.footer').style.display = 'none';
 }
 
 function hideMenu() {
-  // document.querySelector('#mobile-menu').style.display = 'none';
-  document.querySelector('.logo').style.display = 'inline';
-  document.querySelector('.section-container').style.display = 'block';
+  document.querySelector('.mobile-menu').style.display = 'none';
+  //document.querySelector('.logo').style.display = 'inline';
+  //document.querySelector('.section-container').style.display = 'block';
   document.querySelector('.work-container').style.display = 'block';
   document.querySelector('.contact-container').style.display = 'block';
   document.querySelector('.footer').style.display = 'block';
+  
 }
 
 document.getElementById('hamburger-id').addEventListener('click', showMenu);
-document.getElementById('closebtn-id').addEventListener('click', hideMenu);
+document.getElementById('cancel-id').addEventListener('click', hideMenu);
