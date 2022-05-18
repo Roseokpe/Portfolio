56f5654e2f25189f812172e534d0dc2e4012562d
roseokpe <roseokpe@gmail.com>
2022-05-18 10:10:23 +0100

    added hamburger menu

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger');
+// const mobileMenu = document.querySelector('');
+// const aboutContent = document.querySelector('');
+// const modal = document.querySelector('');
+hamburger.addEventListener('click', () =>{
+  hamburger.classList.toggle('active');
+  
+});
