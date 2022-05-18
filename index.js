9fda897888e596892cbddf175ea202e3c11111bb
roseokpe <roseokpe@gmail.com>
2022-05-18 22:32:44 +0100

    adjusted the css style

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.hamburger');
-// const mobileMenu = document.querySelector('');
+ const menuitems = document.querySelector('.menu-list');
 // const aboutContent = document.querySelector('');
 // const modal = document.querySelector('');
   hamburger.addEventListener('click', () =>{
@@ -7,25 +7,21 @@ const hamburger = document.querySelector('.hamburger');
   document.querySelector('.mobile-menu').style.display = 'block';
 });
 
+menuitems.addEventListener('click',() =>{
+menuitems.classList.toggle('hamburger');
+l});
+
 function showMenu() {
-  // document.querySelector('#menuList').style.display = 'inline-block';
-  // document.querySelector('.mobile-sectionLinks').style.display = 'flex';
-  // document.querySelector ('.logo').style.display = 'none';
-  // document.querySelector('.section-container').style.display = 'none';
-  // document.querySelector('.work-container').style.display = 'none';
-  // document.querySelector('.contact-container').style.display = 'none';
-  // document.querySelector('.footer').style.display = 'none';
+  
 }
 
 function hideMenu() {
   document.querySelector('.mobile-menu').style.display = 'none';
-  //document.querySelector('.logo').style.display = 'inline';
-  //document.querySelector('.section-container').style.display = 'block';
   document.querySelector('.work-container').style.display = 'block';
   document.querySelector('.contact-container').style.display = 'block';
   document.querySelector('.footer').style.display = 'block';
-  
 }
 
 document.getElementById('hamburger-id').addEventListener('click', showMenu);
 document.getElementById('cancel-id').addEventListener('click', hideMenu);
+document.querySelector('.menu-list').addEventListener('click', hideMenu);
