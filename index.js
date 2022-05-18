df6ea6b6aee4afcc11ae878ce783f562492a9ca8
tahseenzahra <tahseen1zahra@gmail.com>
2022-05-18 14:54:53 +0500

    Modify js and html file

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ hamburger.addEventListener('click', () =>{
   hamburger.classList.toggle('active');
   
 });
+
+function showMenu() {
+  // document.querySelector('#menuList').style.display = 'inline-block';
+  // document.querySelector('.mobile-sectionLinks').style.display = 'flex';
+  document.querySelector('.logo').style.display = 'none';
+  document.querySelector('.section-container').style.display = 'none';
+  document.querySelector('.work-container').style.display = 'none';
+  document.querySelector('.contact-container').style.display = 'none';
+  document.querySelector('.footer').style.display = 'none';
+}
+
+function hideMenu() {
+  // document.querySelector('#mobile-menu').style.display = 'none';
+  document.querySelector('.logo').style.display = 'inline';
+  document.querySelector('.section-container').style.display = 'block';
+  document.querySelector('.work-container').style.display = 'block';
+  document.querySelector('.contact-container').style.display = 'block';
+  document.querySelector('.footer').style.display = 'block';
+}
+
+document.getElementById('hamburger-id').addEventListener('click', showMenu);
+document.getElementById('closebtn-id').addEventListener('click', hideMenu);
