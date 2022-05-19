b43e136b5d3769f0cebc25633a3465bc591c43c7
roseokpe <roseokpe@gmail.com>
2022-05-19 14:03:44 +0100

    corrected linters errors

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
- const menuitems = document.querySelector('.menu-list');
-hamburger.addEventListener('click', () =>{
+const menuitems = document.querySelector('.menu-list');
+hamburger.addEventListener('click', () => {
   document.querySelector('.mobile-menu').style.display = 'block';
 });
 
-menuitems.addEventListener('click',() =>{
-menuitems.classList.toggle('.mobile-menu');
-l});
+menuitems.addEventListener('click', () => {
+  menuitems.classList.toggle('.mobile-menu');
+});
 
-function showMenu() {
-  
-}
-
-function hideMenu() {
+function hideMenu(){
   document.querySelector('.mobile-menu').style.display = 'none';
   document.querySelector('.work-container').style.display = 'block';
   document.querySelector('.contact-container').style.display = 'block';
   document.querySelector('.footer').style.display = 'block';
 }
 
-document.getElementById('hamburger-id').addEventListener('click', showMenu);
-document.getElementById('cancel-id').addEventListener('click', hideMenu);
-document.querySelector('.menu-list').addEventListener('click', hideMenu);
+document.getElementById('hamburger-id').addEventListener('click', showMenu);document.getElementById('cancel-id').addEventListener('click', hideMenu);document.querySelector('.menu-list').addEventListener('click', hideMenu);
