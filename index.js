504127f8b76044d41dc61f457240ce39a7c85b4b
roseokpe <roseokpe@gmail.com>
2022-05-21 03:15:08 +0100

    I created the pop window

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const menuitems = document.querySelector('.menu-list');
+// const popup = document.querySelector('.popup');
+//  const bttn = document.querySelector('.popup');
+
 hamburger.addEventListener('click', () => {
   document.querySelector('.mobile-menu').style.display = 'block';
 });
+
+// bttn.document.querySelector('click', () => {
+//   document.classList('.popup');
+// });
 
 menuitems.addEventListener('click', () => {
   menuitems.classList.toggle('.mobile-menu');
@@ -22,3 +29,33 @@ function hideMenu() {
 document.getElementById('hamburger-id').addEventListener('click', showMenu);
 document.getElementById('cancel-id').addEventListener('click', hideMenu);
 document.querySelector('.menu-list').addEventListener('click', hideMenu);
+
+// popup 
+
+var modal = document.getElementById("myModal");
+
+// to get the button that opens the modal
+
+(function () {
+  document.querySelectorAll(".raise-modal").forEach(function (trigger) {
+      trigger.addEventListener("click", function () {
+          modal.style.display = "block";
+      });
+  });
+  
+})();
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on (x), it close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
