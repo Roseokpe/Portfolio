213d16ed921a73d8d0423f216e55f0755cc868d9
roseokpe <roseokpe@gmail.com>
2022-05-21 23:01:08 +0100

    made corrections as requested by code reviewer

--- index.js
+++ index.js
@@ -25,10 +25,70 @@ document.getElementById('cancel-id').addEventListener('click', hideMenu);
 document.querySelector('.menu-list').addEventListener('click', hideMenu);
 
 // popup
+const projects = [{
+  id:"001", 
+  name: "Multi - Post stories-1",
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the " +
+  "industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+  image: "images/Snapshoot-Portfolio.png",
+  technologies: ["HTML", "JavaScript", "CSS", "Ruby"],
+  link: "link_to_live_version.com",
+  },
+  {
+    id:"002", 
+    name: "Multi - Post stories-2",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the " +
+    "industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    image: "images/Img-Placeholder.png",
+    technologies: ["HTML", "JavaScript", "CSS", "Ruby"],
+    link: "link_to_live_version.com",
+  },
+  {
+    id:"003", 
+    name: "Multi - Post stories-3",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the " +
+    "industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    image: "images/Snapshoot-Portfolio.png",
+    technologies: ["HTML", "JavaScript", "CSS", "Ruby"],
+    link: "link_to_live_version.com",
+  },
+  {
+    id:"004", 
+    name: "Multi - Post stories-4",
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the " +
+    "industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    image: "images/Snapshoot-Portfolio.png",
+    technologies: ["HTML", "JavaScript", "CSS", "Ruby"],
+    link: "link_to_live_version.com",
+  }
+]
+
+function fillModal(e) {
+  const h2 = document.querySelector('.modal-content h2');
+  const p = document.querySelector('.modal-content p');
+  const img = document.querySelector('.modal-image');
+  const items = document.querySelectorAll('.modal-link li');
+  h2.textContent = e.name; 
+  p.textContent = e.description;
+  img.src = e.image;
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].textContent = e.technologies[i];
+  }
+}
+
+function getModalData(key, obj) {
+  for(var i = 0; i< obj.length; i++) {
+    if(obj[i].id === key) {
+      return obj[i];
+    }
+  }
+}
 const modal = document.getElementById('myModal');
 const raiseModal = document.querySelectorAll('.raise-modal');
 raiseModal.forEach((trigger) => {
   trigger.addEventListener('click', () => {
+    const data = getModalData(trigger.attributes.id.value, projects);
+    fillModal(data);
     modal.style.display = 'block';
   });
 });
