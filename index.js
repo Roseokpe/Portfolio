2f1feae1360d51df0451e1c6e2399949216fa356
roseokpe <roseokpe@gmail.com>
2022-05-21 12:26:09 +0100

    corrected linters errors

--- index.js
+++ index.js
@@ -24,14 +24,12 @@ document.getElementById('hamburger-id').addEventListener('click', showMenu);
 document.getElementById('cancel-id').addEventListener('click', hideMenu);
 document.querySelector('.menu-list').addEventListener('click', hideMenu);
 
-
 //Email validatetion
-
 const myform = document.getElementById('myform');
 const email = document.getElementById('email');
 const error = document.querySelector('.error-msg');
 
-/* the function below check if the email is all lowercase or not*/
+// the function below check if the email address is all lowercase or not //
 function emailCheckLowerCase(e) {
   if (email.value !== email.value.toLowerCase()) {
     error.classList.remove('hide');
