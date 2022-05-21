b94692198325cbf62eb1ba098060de4eadf67f9e
roseokpe <roseokpe@gmail.com>
2022-05-21 10:17:11 +0100

    corrected linters errors

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ const modal = document.getElementById('myModal');
     });
   });
 })();
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-  };
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
 
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName('close')[0];
