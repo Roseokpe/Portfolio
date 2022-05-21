7d3bd9d0e0a432d15c0d593152c5b56fcf8d1c27
roseokpe <roseokpe@gmail.com>
2022-05-21 12:17:36 +0100

    I updated email validation code

--- index.js
+++ index.js
@@ -24,37 +24,14 @@ document.getElementById('hamburger-id').addEventListener('click', showMenu);
 document.getElementById('cancel-id').addEventListener('click', hideMenu);
 document.querySelector('.menu-list').addEventListener('click', hideMenu);
 
-// popup
-const modal = document.getElementById('myModal');
-(function () {
-  document.querySelectorAll('.raise-modal').forEach(function (trigger) {
-    trigger.addEventListener('click', function () {
-      modal.style.display = 'block';
-    });
-  });
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    };
-  };
-})();
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName('close')[0];
-
-// When the user clicks on (x), it close the modal
-span.onclick = function () {
-  modal.style.display = 'none';
-};
 
 //Email validatetion
 
 const myform = document.getElementById('myform');
 const email = document.getElementById('email');
-const error = document.querySelector('.error-message');
-/* the below function will check if the email entered is fully lowerCase */
+const error = document.querySelector('.error-msg');
+
+/* the function below check if the email is all lowercase or not*/
 function emailCheckLowerCase(e) {
   if (email.value !== email.value.toLowerCase()) {
     error.classList.remove('hide');
@@ -63,5 +40,5 @@ function emailCheckLowerCase(e) {
     error.classList.add('hide');
   }
 }
-form.addEventListener('change', emailCheckLowerCase);
-form.addEventListener('submit', emailCheckLowerCase);
+myform.addEventListener('change', emailCheckLowerCase);
+myform.addEventListener('submit', emailCheckLowerCase);
